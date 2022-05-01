b418d4fc73c7447f96fa607f14f4516a1d2462b3
Mohammad Javad Rezaei <mjr0924@gmail.com>
2022-05-01 17:53:36 +0430

    added About & Hero & Navbar & Support component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Support from "./components/Support";
+
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+    </>
   );
 }
 
