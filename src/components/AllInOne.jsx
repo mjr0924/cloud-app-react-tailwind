8b67acd783253286b18209747316d72f75bc4fac
Mohammad Javad Rezaei <mjr0924@gmail.com>
2022-05-01 18:15:06 +0430

    added AllInOne component

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AllInOne = () => {
+  return (
+    <div className="x">
+      <div>
+        <h2>All-In-One Platform</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sint
+          ratione quos at harum, corrupti asperiores optio necessitatibus
+          reprehenderit sunt.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default AllInOne;
