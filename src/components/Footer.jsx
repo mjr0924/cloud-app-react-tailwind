25b235f8b7dafbf8fda84dd5b5e45f0f4e110330
Mohammad Javad Rezaei <mjr0924@gmail.com>
2022-05-04 14:22:48 +0430

    updated Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,81 @@
+import {
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaTwitter,
+  FaTwitch,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="w-full mt-24 bg-slate-900 text-gray-300 py-2 px-2">
+      <div className="max-w-[1240px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
+        <div>
+          <h6 className="font-bold uppercase pt-2">Solutions</h6>
+          <ul>
+            <li className="py-1">Marketing</li>
+            <li className="py-1">Analytics</li>
+            <li className="py-1">Commerce</li>
+            <li className="py-1">Data</li>
+            <li className="py-1">Cloud</li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 className="font-bold uppercase pt-2">SUPPORT</h6>
+          <ul>
+            <li className="py-1">Pricing</li>
+            <li className="py-1">Documentation</li>
+            <li className="py-1">Guides</li>
+            <li className="py-1">API Status</li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 className="font-bold uppercase pt-2">COMPANY</h6>
+          <ul>
+            <li className="py-1">About</li>
+            <li className="py-1">Blog</li>
+            <li className="py-1">Jobs</li>
+            <li className="py-1">Press</li>
+            <li className="py-1">Partners</li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 className="font-bold uppercase pt-2">LEGAL</h6>
+          <ul>
+            <li className="py-1">Claims</li>
+            <li className="py-1">Privacy</li>
+            <li className="py-1">Terms</li>
+            <li className="py-1">Policies</li>
+            <li className="py-1">Conditions</li>
+          </ul>
+        </div>
+
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className="font-bold uppercase">SUBSCRIBE TO OUR NEWSLETTER</p>
+          <p className="py-4">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <form className="flex flex-col sm:flex-row">
+            <input className="w-full p-2 mr-4 rounded-md mb-4" type="email" placeholder="Enter Email" />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
+      </div>
+      <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
+        <p className="py-4">2022 Workflow, LLC. All rights reserved</p>
+        <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+          <FaFacebook />
+          <FaInstagram />
+          <FaGithub />
+          <FaTwitch />
+          <FaTwitter />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { MenuIcon, XIcon, xIcon } from "@heroicons/react/outline";
 import { useState } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -10,11 +11,13 @@ const Navbar = () => {
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
           <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Platforms</li>
-            <li>Pricing</li>
+            
+            <li><Link to="test1" smooth={true} offset={50} duration={500}>Home</Link></li>
+            <li><Link to="test1" smooth={true} offset={50} duration={500}>About</Link></li>
+            <li><Link to="test1" smooth={true} offset={50} duration={500}>Support</Link></li>
+            <li><Link to="test1" smooth={true} offset={50} duration={500}>Platforms</Link></li>
+            <li><Link to="test1" smooth={true} offset={50} duration={500}>Pricing</Link></li>
+            
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
